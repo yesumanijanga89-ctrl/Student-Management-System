fad0d83d206f043bceec0889ba5465a5b53c2d52
Yesumani . Janga <yesumanijanga89@gmail.com>
2026-07-21 16:18:27 +0530

    Create 02_Insert_Data.sql

---
+++ 02_Insert_Data.sql
@@ -0,0 +1,15 @@
+INSERT INTO Students VALUES
+(101,'Ravi','Male',20,'B.Sc'),
+(102,'Sita','Female',21,'B.Com'),
+(103,'Kiran','Male',22,'BCA');
+
+INSERT INTO Subjects VALUES
+(1,'SQL'),
+(2,'Python'),
+(3,'Java');
+
+INSERT INTO Marks VALUES
+(1,101,1,85),
+(2,101,2,90),
+(3,102,1,88),
+(4,103,3,92);
